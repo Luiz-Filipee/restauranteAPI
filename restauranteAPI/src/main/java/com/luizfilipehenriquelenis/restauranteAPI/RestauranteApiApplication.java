95dbd83e8c7a345f5d4a7d5318a4fb9c8c900ebf
Luiz-Filipee <luizkato7@gmail.com>
2024-10-16 19:35:41 -0400

    primeiro commmit

---
+++ restauranteAPI/src/main/java/com/luizfilipehenriquelenis/restauranteAPI/RestauranteApiApplication.java
@@ -0,0 +1,13 @@
+package com.luizfilipehenriquelenis.restauranteAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteApiApplication.class, args);
+	}
+
+}
